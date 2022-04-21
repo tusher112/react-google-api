f5eba637a6e89b16ac88f261b31527580f3551a0
tusher112 <mdtusherahmed820@gmail.com>
2022-04-21 21:29:29 +0600

    google map api field added

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Mylocation from './components/Mylocation';
+
+function App() {
+  return (
+    <div className="App">
+      <Mylocation></Mylocation>
+    </div>
+  );
+}
+
+export default App;
